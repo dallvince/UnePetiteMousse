ad161cdb957fcca7c61a1ec00a84a16d43bb508c
Nelson PIRES DA SILVA <piresdasilva.n@gmail.com>
2022-05-10 09:48:22 +0200

    pays

--- src/Form/BrewriesType.php
+++ src/Form/BrewriesType.php
@@ -3,9 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Brewries;
+use App\Entity\Countries;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class BrewriesType extends AbstractType
 {
@@ -14,7 +17,11 @@ class BrewriesType extends AbstractType
         $builder
             ->add('name')
             ->add('description')
-            ->add('countries')
+            ->add('countries', EntityType::class, [
+                "class" => Countries::class,
+                "placeholder" => "Choisissez un pays",
+                "required" => false,
+            ])
         ;
     }
 
